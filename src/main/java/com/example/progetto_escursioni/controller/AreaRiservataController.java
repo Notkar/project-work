609b06e443c87885de0f8afbf4feaa656905bedb
Giacomo-Mazzoni <giacomo-mazzoni@outlook.it>
2024-11-20 19:46:21 +0100

    implementazione modifica dati utente

--- src/main/java/com/example/progetto_escursioni/controller/AreaRiservataController.java
+++ src/main/java/com/example/progetto_escursioni/controller/AreaRiservataController.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Controller
@@ -30,13 +32,18 @@ public class AreaRiservataController {
     @GetMapping
     public String getPage(HttpSession session,
                           Model model,
-                          @RequestParam(name = "candidatura", required = false) String successoCandidatura) {
+                          @RequestParam(name = "candidatura", required = false) String successoCandidatura,
+                          @RequestParam(name = "successoModifica", required = false) String successoModifica) {
 
         // check per vedere se c'è un utente in sessione, altrimenti fa redirect a loginregistrazione
         if (session.getAttribute("utente") != null) {
             // recupero utente da session e lo salvo nel model per stamparne le variabili tramite thymeleaf
             Utente utente = (Utente) session.getAttribute("utente");
             model.addAttribute("utente", utente);
+            // recupero la data di nascita dell'utente e la salvo opportunamente formattata nel model per il form di modifica dati
+            DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String dataNascitaFormattataPerCampo = utente.getDataNascita().format(pattern);
+            model.addAttribute("dataNascitaFormattata", dataNascitaFormattataPerCampo);
 
             // creo oggetto candidato per poterci fare il binding con il form per le candidature
             Candidato candidato = new Candidato();
@@ -51,6 +58,13 @@ public class AreaRiservataController {
                 model.addAttribute("messaggio", "Candidatura inviata con successo: ti risponderemo al più presto!");
             } else if (successoCandidatura != null && successoCandidatura.equals("false")){
                 model.addAttribute("messaggio", "Non puoi inviare un'altra candidatura!"); // caso che non dovrebbe essere possibile, lasciato per sicurezza
+            }
+
+            // messaggi di successo o errore per modifica dati utente
+            if(successoModifica != null && successoModifica.equals("true")){
+                model.addAttribute("messaggio", "Modifica dati utente effettuata con successo");
+            } else if (successoModifica != null && successoModifica.equals("false")){
+                model.addAttribute("messaggio", "Modifica dati utente fallita: username o email già in uso");
             }
 
             // se c'è una candidatura associata all'utente registro nel model una stringa per far comparire lo script per disabilitare il tasto di candidatura
@@ -101,6 +115,45 @@ public class AreaRiservataController {
         Utente utente = (Utente)session.getAttribute("utente");
         // ritorno true o false a seconda che l'input dell'utente sia uguale o meno alla password recuperata con il metodo getter getPassword()
         return inputPassword.equals(utente.getPassword());
+    }
+
+    @PostMapping("/modificaDati")
+    public String modificaDati(@RequestParam("dataNascita") String dataNascitaString,
+                               Model model,
+                               @ModelAttribute("utente") Utente utente,
+                               HttpSession session) {
+        Utente utenteDaSession = (Utente)session.getAttribute("utente"); // recuperiamo l'utente pre-modifiche dalla session
+
+        boolean usernameIdentico = utente.getUsername().equals(utenteDaSession.getUsername());
+        boolean emailIdentica = utente.getEmail().equals(utenteDaSession.getEmail());
+
+        if(!(usernameIdentico && emailIdentica) && usernameIdentico) {
+            if(!utenteService.controlloUsernameEmail("", utente.getEmail())){
+                System.out.println("username uguale: controllo email fallito");
+                return "redirect:/areariservata?successoModifica=false";
+            }
+        } else if(!(usernameIdentico && emailIdentica) && emailIdentica) {
+            if(!utenteService.controlloUsernameEmail(utente.getUsername(), "")){
+                System.out.println("email uguale: controllo username fallito");
+                return "redirect:/areariservata?successoModifica=false";
+            }
+        } else if(!usernameIdentico && !emailIdentica) {
+            if(!utenteService.controlloUsernameEmail(utente.getUsername(), utente.getEmail())){
+                System.out.println("username & email uguali: controllo username o email fallito");
+                return "redirect:/areariservata?successoModifica=false";
+            }
+        }
+
+        System.out.println("controlli passati");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate dataNascita = LocalDate.parse(dataNascitaString, formatter);
+        utente.setDataNascita(dataNascita);
+
+        utenteService.salvaUtente(utente); // se è tutto ok salva l'utente sul database
+        session.setAttribute("utente", utente); // sovrascriviamo l'utente in session con l'utente appena modificato
+
+        return "redirect:/areariservata?successoModifica=true";
     }
 
 }
